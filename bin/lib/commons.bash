971b3e39deb6e9bfe9a06533e8a9a0f6b88ceef1
Dominique Adolphe <domiado16@gmail.com>
2020-07-12 10:05:35 +0400

    2020-07-12-10-05

--- bin/lib/commons.bash
+++ bin/lib/commons.bash
@@ -17,3 +17,21 @@ function hasAtLeastOneArgument
 {
     [[ "$#" != "0" ]]
 }
+
+function hasMoreThanOneArgument
+{
+    [[ "$#" != "0" ]] && [[ "$#" != "1" ]]
+}
+
+function doesBranchExist
+{
+    local BRANCH_NAME="$1"
+    [[ ! -z "$(git branch --list "$BRANCH_NAME")" ]]
+}
+
+function getLastCommitMessage
+{
+    local BRANCH_NAME="$1"
+    doesBranchExist "$BRANCH_NAME"
+    [[ "$?" == "0" ]] && git log -1  --pretty='%s' "$BRANCH_NAME"
+}
